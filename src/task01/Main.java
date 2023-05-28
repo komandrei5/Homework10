983f36dad0321ca9559c9da7982f060c2af10232
Andriy Svidlo <komandrei5@gmail.com>
2023-05-28 17:34:05 +0300

    Initial commit

---
+++ src/task01/Main.java
@@ -0,0 +1,16 @@
+package task01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Task01 task = new Task01();
+        List<String> words = Arrays.asList("apple", "banana", "orange", "apple", "grape", "apple",
+        "pen", "Hi", "read", "pear", "table", "potato", "plume", "run!", "Go");
+
+        List<String> randomWords = task.getList(words, 7771);
+        int occurrences = task.countOccurance(randomWords, "Go");
+        System.out.println("Occurrences of coincidence: "  + occurrences);
+    }
+}
---
+++ src/task01/Task01.java
@@ -0,0 +1,30 @@
+package task01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task01 {
+
+    public int countOccurance(List<String> list, String pattern) {
+        int count = 0;
+        for (String word : list) {
+            if (word.equals(pattern)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+     List<String> getList (List<String> words, int count) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            String word = getRandomWord(words);
+            result.add(word);
+        }
+        return result;
+    }
+
+    private String getRandomWord(List<String> words) {
+        return words.get((int) (Math.random()*15));
+    }
+}
